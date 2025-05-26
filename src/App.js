b20b49911ae37fc0b7bf0684a2e39fc8aaa8bfbf
Sharda Singh <shardasingh110011@gmail.com>
2025-05-26 10:50:50 +0530

    install and configure Tailwind CSS

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     <h1> Hello Learn and track</h1>
+      <div className="bg-orange-400 text-4xl text-white p-4 rounded-lg">
+        Hello Tailwind!
+      </div>
+      <h1 className="text-4xl text-blue-700 font-bold"> Hello Learn and track</h1>
     </div>
   );
 }
